e816942c7b46e200f1cbdea28bc3e560d03f0918
ariel murphy <arielladders@gmail.com>
2020-07-10 17:05:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.last "arrays!"
+  array.last ["arrays!"]
 end
 
 def add_element_to_start_of_array(array, element)
-   array.first  "wow"
+   array.first  ["wow"]
 end
 
 def remove_element_from_end_of_array(array)
