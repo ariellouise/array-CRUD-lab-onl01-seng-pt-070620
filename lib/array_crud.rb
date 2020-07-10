bd205335f18ea33610bb01a9604e6294a3b5177e
ariel murphy <arielladders@gmail.com>
2020-07-10 16:54:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  create_an_empty_array = []
+ empty_array = []
 end
 
 def create_an_array
-  create_an_array = [mothman, chupacabra, lochness_monster, bigfoot]
+  cryptids = [mothman, chupacabra, lochness_monster, bigfoot]
 end
 
 def add_element_to_end_of_array(array, element)
